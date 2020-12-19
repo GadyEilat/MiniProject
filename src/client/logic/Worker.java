8b5573b0fbbed74e38e9dfb20947d5c667dc11e2
Gady <GadyEilat@gmail.com>
2020-12-19 19:56:07 +0200

    added "role" field to Worker entity

--- src/client/logic/Worker.java
+++ src/client/logic/Worker.java
@@ -2,16 +2,18 @@ package client.logic;
 
 import java.io.Serializable;
 
-public class Worker implements Serializable{
+public class Worker implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private String userName;
 	private String password;
-	
-	public Worker(String userName, String password) {
+	private String role;
+
+	public Worker(String userName, String password, String role) {
 		this.userName = userName;
 		this.password = password;
+		this.role = role;
 	}
 
 	public String getUserName() {
@@ -30,10 +32,17 @@ public class Worker implements Serializable{
 		this.password = password;
 	}
 
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
 //	@Override
 //	public String toString() {
 //		return "Worker [userName=" + userName + ", password=" + password + "]";
 //	}
-
 
 }
