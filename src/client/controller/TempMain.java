d9a8e8de6515d3309a350f7a9b59e356fa5ee4d7
Gady <GadyEilat@gmail.com>
2020-12-23 15:33:41 +0200

    a

--- src/client/controller/TempMain.java
+++ src/client/controller/TempMain.java
@@ -24,11 +24,11 @@ public class TempMain extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		TempMain.primaryStage = primaryStage;
-		//TempMain.primaryStage.setTitle("Existing Order");
 		Parent current;
 		try {
 			TempMain.fxmlLoader = new FXMLLoader(getClass().getResource("/client/boundaries/Existing Order.fxml"));
 			current = (Parent)fxmlLoader.load();
+			System.out.println("a");
 			Scene scene = new Scene(current);
 			scene.getStylesheets().add("client/boundaries/ManagerStyle.css");
 			TempMain.primaryStage.setScene(scene); 
