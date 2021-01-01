c6434d3eb1967830997f2fc1a5f25f77458efc97
דניאלה <@DESKTOP-5A4A01G>
2021-01-01 18:46:37 +0200

    edit info sub

--- src/client/logic/Subscriber.java
+++ src/client/logic/Subscriber.java
@@ -5,10 +5,31 @@ public class Subscriber extends Visitor{
 	private static final long serialVersionUID = 1L;
 
 	private String amountOfFamilyMember;
+	private String CreditCard;
+	private String subscriberNumber;
+	public Subscriber() {super(null, null, null, null, null);}
 	
-	public Subscriber(String id, String fname, String sname, String email, String teln, String amountOfFamilyMember) {
+	public Subscriber(String id, String fname, String sname, String email, String teln, String amountOfFamilyMember, String CreditCard, String subscriberNumber) {
 		super(id, fname, sname, email, teln);
 		this.amountOfFamilyMember=amountOfFamilyMember;
+		this.CreditCard = CreditCard;
+		this.subscriberNumber = subscriberNumber;
+	}
+
+	public String getCreditCard() {
+		return CreditCard;
+	}
+
+	public void setCreditCard(String creditCard) {
+		CreditCard = creditCard;
+	}
+
+	public String getSubscriberNumber() {
+		return subscriberNumber;
+	}
+
+	public void setSubscriberNumber(String subscriberNumber) {
+		this.subscriberNumber = subscriberNumber;
 	}
 
 	public String getAmountOfFamilyMember() {
--- src/server/EchoServer.java
+++ src/server/EchoServer.java
@@ -10,6 +10,7 @@ import java.util.Vector;
 import client.ClientUI;
 import client.logic.Order;
 import client.logic.ParkInfo;
+import client.logic.Subscriber;
 import client.logic.TourGuide;
 import client.logic.TourGuideOrder;
 import client.logic.Visitor;
@@ -104,9 +105,50 @@ public class EchoServer extends AbstractServer {
 			}
 			break;
 		case REQUESTINFO:
+			if (object instanceof Subscriber) {
+				Subscriber subscriber = (Subscriber) object;
+				String checkSubExist = "SELECT * FROM gonature.subscriber WHERE subscriberNumber ='"+subscriber.getSubscriberNumber()+"';";
+				arrOfAnswer = mysqlConnection.getDB(checkSubExist);
+				
+				if (!arrOfAnswer.isEmpty()) {
+					subscriber.setId((String) arrOfAnswer.get(0));
+					subscriber.setFname((String) arrOfAnswer.get(1));
+					subscriber.setLname((String) arrOfAnswer.get(2));
+					subscriber.setEmail((String) arrOfAnswer.get(3));
+					subscriber.setteln((String) arrOfAnswer.get(4));
+					subscriber.setAmountOfFamilyMember((String) arrOfAnswer.get(5));
+					subscriber.setCreditCard((String) arrOfAnswer.get(6));
+					subscriber.setSubscriberNumber((String) arrOfAnswer.get(7));	
+
+					returnData = new DataTransfer(TypeOfMessageReturn.REQUESTINFO_SUCCESS, subscriber);
+					try {
+						client.sendToClient(returnData);
+
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				} else
+					ServerController.instance.displayMsg("subscriber REQUESTINFO details failed");
+											
+			}
 
 			break;
 		case UPDATEINFO:
+			if (object instanceof Subscriber) {
+				Subscriber subscriber = (Subscriber) object;
+				String upDateSub = "UPDATE `gonature`.`subscriber` SET `ID` = '"+subscriber.getId()+"', `FirstName` = '"+subscriber.getFname()+"', "
+						+ "`LastName` = '"+subscriber.getLname()+"', `Email` = '"+subscriber.getEmail()+"', `Telephone` = '"+subscriber.getTeln()+"', "
+						+ "`AmountOfFamilyMembers` = '"+subscriber.getAmountOfFamilyMember()+"', `CreditCard` = '"+subscriber.getCreditCard()+"' WHERE "
+								+ "(`ID` = '"+subscriber.getId()+"') and " + "(`subscriberNumber` = '"+subscriber.getSubscriberNumber()+"');";
+				boolean Answer = mysqlConnection.updateDB(upDateSub);
+				if(Answer) {
+					ServerController.instance.displayMsg("subscriber UPDATEINFO details updated");
+
+				} else {
+					ServerController.instance.displayMsg("subscriber UPDATEINFO details failed");
+				}
+				
+			}
 
 			break;
 		case LOGIN_REQUEST:
@@ -114,9 +156,9 @@ public class EchoServer extends AbstractServer {
 				Worker worker = (Worker) object;
 				Worker RoleAndPark = null;
 				ParkInfo parkInfo;
-				String str = "SELECT Role, Park, name FROM gonature.worker WHERE UserName = '" + worker.getUserName()
+				String checkUserAndPassword = "SELECT Role, Park, name FROM gonature.worker WHERE UserName = '" + worker.getUserName()
 						+ "' AND Password = '" + worker.getPassword() + "';";
-				arrOfAnswer = mysqlConnection.getDB(str);
+				arrOfAnswer = mysqlConnection.getDB(checkUserAndPassword);
 				if (!arrOfAnswer.isEmpty()) {
 					String scene;
 					String role = (String) arrOfAnswer.get(0);
@@ -149,7 +191,7 @@ public class EchoServer extends AbstractServer {
 
 					returnData = new DataTransfer(TypeOfMessageReturn.LOGIN_SUCCESSFUL, RoleAndPark);
 				} else
-					returnData = new DataTransfer(TypeOfMessageReturn.LOGIN_FAILED, null);
+					returnData = new DataTransfer(TypeOfMessageReturn.LOGIN_FAILED, new Worker());
 				try {
 					client.sendToClient(returnData);
 
@@ -157,6 +199,38 @@ public class EchoServer extends AbstractServer {
 					e.printStackTrace();
 				}
 			}
+			
+			
+			
+			if (object instanceof Subscriber) {
+				Subscriber subscriber = (Subscriber) object;
+				String checkSubExist = "SELECT * FROM gonature.subscriber WHERE subscriberNumber ='"+subscriber.getSubscriberNumber()+"' "
+						+ "OR ID = '"+subscriber.getId()+"'; ";
+				arrOfAnswer = mysqlConnection.getDB(checkSubExist);
+				
+				if (!arrOfAnswer.isEmpty()) {
+					subscriber.setId((String) arrOfAnswer.get(0));
+					subscriber.setFname((String) arrOfAnswer.get(1));
+					subscriber.setLname((String) arrOfAnswer.get(2));
+					subscriber.setEmail((String) arrOfAnswer.get(3));
+					subscriber.setteln((String) arrOfAnswer.get(4));
+					subscriber.setAmountOfFamilyMember((String) arrOfAnswer.get(5));
+					subscriber.setCreditCard((String) arrOfAnswer.get(6));
+					subscriber.setSubscriberNumber((String) arrOfAnswer.get(7));	
+
+					returnData = new DataTransfer(TypeOfMessageReturn.LOGIN_SUCCESSFUL, subscriber);
+				} else
+					returnData = new DataTransfer(TypeOfMessageReturn.LOGIN_FAILED, new Subscriber());
+				try {
+					client.sendToClient(returnData);
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+							
+			}
+			
+			
 			break;
 		case LOGOUT:
 
