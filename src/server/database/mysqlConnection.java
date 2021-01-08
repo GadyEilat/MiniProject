ffc5d424a6f1f9ef4f90f9ab9d5c24e0ac6a8141
AVIV\ASUS <kamaryaviv@gmail.com>
2021-01-09 01:30:06 +0200

    svd

--- src/server/database/mysqlConnection.java
+++ src/server/database/mysqlConnection.java
@@ -43,9 +43,9 @@ public class mysqlConnection {
 		try {
 
 
-			conn = DriverManager.getConnection("jdbc:mysql://localhost/gonature?serverTimezone=IST", "root","ha89kha89k");
+	//		conn = DriverManager.getConnection("jdbc:mysql://localhost/gonature?serverTimezone=IST", "root","ha89kha89k");
 //			conn = DriverManager.getConnection("jdbc:mysql://localhost/gonature?serverTimezone=IST", "root","Liran159357!");
-//			conn = DriverManager.getConnection("jdbc:mysql://localhost/gonature?serverTimezone=IST", "root","Aa123456");
+		conn = DriverManager.getConnection("jdbc:mysql://localhost/gonature?serverTimezone=IST", "root","Aa123456");
 //			conn = DriverManager.getConnection("jdbc:mysql://localhost/gonature?serverTimezone=IST", "root","DA123456");
 
 
